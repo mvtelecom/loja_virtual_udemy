40813e00c9477488c0fd9118319470f5c98cfc28
marcus <mv.telecom20@gmail.com>
2020-07-15 13:23:54 -0300

    165

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,120 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { CieloConstructor, Cielo, TransactionCreditCardRequestModel, CaptureRequestModel, CancelTransactionRequestModel, EnumBrands} from 'cielo';
+
+admin.initializeApp(functions.config().firebase);
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+const merchantId = functions.config().cielo.merchantId;
+const merchantKey = functions.config().cielo.merchantKey;
+
+const cieloParams: CieloConstructor = {
+    merchantId: merchantId,
+    merchantKey: merchantKey,
+    sandbox: true,
+    debug: true,
+}
+
+const cielo = new Cielo(cieloParams);
+
+export const authorizeCreditCard = functions.https.onCall(async (data, context) => {
+    if(data === null){
+        return {
+            "success": false,
+            "error": {
+                "code": -1,
+                "message": "Dados não informados"
+            }
+        };
+    }
+
+    if(!context.auth){
+        return {
+            "success": false,
+            "error": {
+                "code": -1,
+                "message": "Nenhum usuário logado"
+            }
+        };
+    }
+
+    const userId = context.auth.uid;
+
+    const snapshot = await admin.firestore().collection("users").doc(userId).get();
+    const userData = snapshot.data;
+
+    console.log("Iniciando Autorização");
+
+    let brand: EnumBrands;
+    switch(data.creditCard.brand){
+        case "VISA":
+            brand = EnumBrands.VISA;
+            break;
+        case "MASTERCARD":
+            brand = EnumBrands.MASTER;
+            break;
+        case "AMEX":
+            brand = EnumBrands.AMEX;
+            break;
+        case "ELO":
+            brand = EnumBrands.ELO;
+            break;
+        case "JCB":
+            brand = EnumBrands.JCB;
+            break;
+        case "DINERSCLUB":
+            brand = EnumBrands.DINERS;
+            break;
+        case "DISCOVER":
+            brand = EnumBrands.DISCOVERY;
+            break;
+        case "HIPERCARD":
+            brand = EnumBrands.HIPERCARD;
+            break;
+    }
+
+});
+
+
+
+
+
+
+
+export const helloWorld = functions.https.onCall((data, context) => {
+  return {data: "Hellow from Cloud Functions!!!"};
+});
+
+export const getUserData = functions.https.onCall( async (data, context) => {
+    if(!context.auth){
+        return {
+            "data": "Nenhum usuário logado"
+        };
+    }
+
+    console.log(context.auth.uid);
+
+    const snapshot = await admin.firestore().collection("users").doc(context.auth.uid).get();
+
+    console.log(snapshot.data());
+
+    return {
+        "data": snapshot.data()
+    };
+});
+
+export const addMessage = functions.https.onCall( async (data, context) => {
+    console.log(data);
+
+    const snapshot = await admin.firestore().collection("messages").add(data);
+
+    return {"success": snapshot.id};
+});
+
+export const onNewOrder = functions.firestore.document("/orders/{orderId}").onCreate((snapshot, context) => {
+    const orderId = context.params.orderId;
+    console.log(orderId);
+});
