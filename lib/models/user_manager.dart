a2e6d2ae8259feda7f92d34e8fee5d8f791d1315
marcus <mv.telecom20@gmail.com>
2020-07-14 23:29:55 -0300

    136

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:lojavirtual/helpers/firebase_errors.dart';
 import 'package:lojavirtual/models/user.dart';
 
@@ -18,6 +19,17 @@ class UserManager extends ChangeNotifier {
 
   bool _loading = false;
   bool get loading => _loading;
+  set loading(bool value){
+    _loading = value;
+    notifyListeners();
+  }
+
+  bool _loadingFace = false;
+  bool get loadingFace => _loadingFace;
+  set loadingFace(bool value){
+    _loadingFace = value;
+    notifyListeners();
+  }
 
   bool get isLoggedIn => user != null;
 
@@ -34,6 +46,43 @@ class UserManager extends ChangeNotifier {
       onFail(getErrorString(e.code));
     }
     loading = false;
+  }
+
+  Future<void> facebookLogin({Function onFail, Function onSuccess}) async {
+    loadingFace = true;
+
+    final result = await FacebookLogin().logIn(['email', 'public_profile']);
+
+    switch(result.status){
+      case FacebookLoginStatus.loggedIn:
+        final credential = FacebookAuthProvider.getCredential(
+          accessToken: result.accessToken.token
+        );
+
+        final authResult = await auth.signInWithCredential(credential);
+
+        if(authResult.user != null){
+          final firebaseUser = authResult.user;
+
+          user = User(
+            id: firebaseUser.uid,
+            name: firebaseUser.displayName,
+            email: firebaseUser.email
+          );
+
+          await user.saveData();
+
+          onSuccess();
+        }
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        break;
+      case FacebookLoginStatus.error:
+        onFail(result.errorMessage);
+        break;
+    }
+
+    loadingFace = false;
   }
 
   Future<void> signUp({User user, Function onFail, Function onSuccess}) async {
@@ -57,11 +106,6 @@ class UserManager extends ChangeNotifier {
   void signOut(){
     auth.signOut();
     user = null;
-    notifyListeners();
-  }
-
-  set loading(bool value){
-    _loading = value;
     notifyListeners();
   }
 
